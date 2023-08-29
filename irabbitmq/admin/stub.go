cc603657e1faa11e45dc4c65498668fc950ee011
byt3-m3 <cbaxtertech@gmail.com>
2023-08-28 23:51:21 -0400

    refactored stub

--- irabbitmq/admin/stub.go
+++ irabbitmq/admin/stub.go
@@ -6,85 +6,66 @@ import (
 )
 
 type StubClient struct {
-	CreateQueueStubReturn    func(ctx context.Context, input *CreateQueueInput) CreateQueueStubReturn
-	CreateExchangeStubReturn func(ctx context.Context, input *CreateExchangeInput) CreateExchangeStubReturn
-	BindQueueStubReturn      func(ctx context.Context, input *BindQueueInput) BindQueueStubReturn
-	DeleteQueueStubReturn    func(ctx context.Context, input *DeleteQueueInput) DeleteQueueStubReturn
-	DeleteExchangeStubReturn func(ctx context.Context, input *DeleteExchangeInput) DeleteExchangeStubReturn
-	GetConnectionStubReturn  func() GetConnectionStubReturn
+	CreateQueueStubReturn    func(ctx context.Context, input *CreateQueueInput) (*amqp091.Queue, error)
+	CreateExchangeStubReturn func(ctx context.Context, input *CreateExchangeInput) error
+	BindQueueStubReturn      func(ctx context.Context, input *BindQueueInput) error
+	DeleteQueueStubReturn    func(ctx context.Context, input *DeleteQueueInput) error
+	DeleteExchangeStubReturn func(ctx context.Context, input *DeleteExchangeInput) error
+	GetConnectionStubReturn  func() *amqp091.Connection
+	WithAMQPUrlStubReturn    func(url string)
+	WithVHostStubReturn      func(vhost string)
+	WithPlainAuthStubReturn  func(username, password string)
+	WithConnectionStubReturn func(conn *amqp091.Connection)
+	ValidateClientStubReturn func(client *adminClient) bool
 }
 
-func (s *StubClient) WithAMQPUrl(url string) *adminClient {
-	//TODO implement me
-	panic("implement me")
+func (s *StubClient) WithAMQPUrl(url string) Client {
+	s.WithAMQPUrlStubReturn(url)
+	return s
 }
 
-func (s *StubClient) WithVHost(vhost string) *adminClient {
-	//TODO implement me
-	panic("implement me")
+type WithVHostStubReturn struct {
 }
 
-func (s *StubClient) WithPlainAuth(username, password string) *adminClient {
-	//TODO implement me
-	panic("implement me")
+func (s *StubClient) WithVHost(vhost string) Client {
+	s.WithVHostStubReturn(vhost)
+	return s
 }
 
-func (s *StubClient) WithConnection(conn *amqp091.Connection) *adminClient {
-	//TODO implement me
-	panic("implement me")
+func (s *StubClient) WithPlainAuth(username, password string) Client {
+	s.WithPlainAuthStubReturn(username, password)
+	return s
+}
+
+func (s *StubClient) WithConnection(conn *amqp091.Connection) Client {
+	s.WithConnectionStubReturn(conn)
+	return s
 }
 
 func (s *StubClient) ValidateClient(client *adminClient) bool {
-	//TODO implement me
-	panic("implement me")
-}
-
-type CreateQueueStubReturn struct {
-	Queue *amqp091.Queue
-	Err   error
+	return s.ValidateClientStubReturn(client)
 }
 
 func (s *StubClient) CreateQueue(ctx context.Context, input *CreateQueueInput) (*amqp091.Queue, error) {
-	res := s.CreateQueueStubReturn(ctx, input)
-	return res.Queue, res.Err
-}
-
-type CreateExchangeStubReturn struct {
-	Error error
+	return s.CreateQueueStubReturn(ctx, input)
 }
 
 func (s *StubClient) CreateExchange(ctx context.Context, input *CreateExchangeInput) error {
-	return s.CreateExchangeStubReturn(ctx, input).Error
-}
-
-type BindQueueStubReturn struct {
-	Error error
+	return s.CreateExchangeStubReturn(ctx, input)
 }
 
 func (s *StubClient) BindQueue(ctx context.Context, input *BindQueueInput) error {
-	return s.BindQueueStubReturn(ctx, input).Error
-}
-
-type GetConnectionStubReturn struct {
-	Conn *amqp091.Connection
+	return s.BindQueueStubReturn(ctx, input)
 }
 
 func (s *StubClient) GetConnection() *amqp091.Connection {
-	return s.GetConnectionStubReturn().Conn
-}
-
-type DeleteQueueStubReturn struct {
-	Error error
+	return s.GetConnectionStubReturn()
 }
 
 func (s *StubClient) DeleteQueue(ctx context.Context, input *DeleteQueueInput) error {
-	return s.DeleteQueueStubReturn(ctx, input).Error
-}
-
-type DeleteExchangeStubReturn struct {
-	Error error
+	return s.DeleteQueueStubReturn(ctx, input)
 }
 
 func (s *StubClient) DeleteExchange(ctx context.Context, input *DeleteExchangeInput) error {
-	return s.DeleteExchangeStubReturn(ctx, input).Error
+	return s.DeleteExchangeStubReturn(ctx, input)
 }
