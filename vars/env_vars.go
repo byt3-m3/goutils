7ee824463ab1d94b6d0ae0a13689bb27647362ee
byt3-m3 <cbaxtertech@gmail.com>
2024-05-05 10:53:22 -0400

    removed files

--- vars/env_vars.go
+++
@@ -1,9 +0,0 @@
-package vars
-
-import "github.com/byt3-m3/goutils/env_utils"
-
-var (
-	LogLevel      = env_utils.GetEnv("LOG_LEVEL", "info")
-	KafkaUsername = env_utils.GetEnv("KAFKA_USERNAME", "user")
-	KafkaPassword = env_utils.GetEnv("KAFKA_PASSWORD", "pass")
-)
